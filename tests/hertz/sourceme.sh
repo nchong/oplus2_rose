9b0ad3dd9cfa4dd2c1afcfcd4b504b02ef49b814
Nathan Chong <nyc04@doc.ic.ac.uk>
2011-05-03 22:41:05 +0100

    Remove machine/login-specific sourceme.sh

--- tests/hertz/sourceme.sh
+++
@@ -1,5 +0,0 @@
-export FRAMEWORKDIR=/homes/nyc04/work/hertz_framework
-export DATADIR=${FRAMEWORKDIR}/data
-export HERTZ_COMMONDIR=${FRAMEWORKDIR}/common
-export ROSEDIR=/data/nyc04/ROSE_INSTALL/
-export LD_LIBRARY_PATH=/data/nyc04/ROSE_INSTALL/lib:$LD_LIBRARY_PATH
